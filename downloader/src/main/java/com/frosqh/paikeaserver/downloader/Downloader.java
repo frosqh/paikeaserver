53562299a1d9e43727de7e551eabfcd66024adb7
Gautier Raimondi <gautier.raimondi@inria.fr>
2020-12-13 15:22:51 +0100

    Adding output stream

--- downloader/src/main/java/com/frosqh/paikeaserver/downloader/Downloader.java
+++ downloader/src/main/java/com/frosqh/paikeaserver/downloader/Downloader.java
@@ -6,8 +6,7 @@ import com.frosqh.paikeaserver.settings.Settings;
 import com.github.kiulian.downloader.YoutubeDownloader;
 import com.mpatric.mp3agic.*;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,6 +15,31 @@ public class Downloader extends YoutubeDownloader {
     private final Map<String, Integer> percentageDownloads = new HashMap<>();
     private static Downloader instance;
     private static String response;
+
+    class StreamGobbler extends Thread {
+        InputStream is;
+        String type;
+
+
+        StreamGobbler (InputStream is, String type){
+            this.is = is;
+            this.type = type;
+        }
+
+        public void run(){
+            try {
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    System.out.println(type + ">" + line);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     private Downloader(){
         super();
@@ -31,6 +55,12 @@ public class Downloader extends YoutubeDownloader {
         System.out.println(id + "-" + title + "-" + artist);
         try {
             Process p = Runtime.getRuntime().exec("youtube-dl "+id+" -x --audio-format mp3 --id");
+            StreamGobbler errorGobbler = new StreamGobbler(p.getErrorStream(), "ERROR");
+            StreamGobbler outputGobbler = new StreamGobbler(p.getInputStream(), "OUTPUT");
+
+            errorGobbler.start();
+            outputGobbler.start();
+
             p.waitFor();
             String current = System.getProperty("user.dir");
             String mp3Name = current+"/"+id+".mp3";
@@ -53,6 +83,12 @@ public class Downloader extends YoutubeDownloader {
     public void downloadFromYoutubePlayListFirstStep(String id){
         try {
             Process p = Runtime.getRuntime().exec("youtube-dl -x --audio-format mp3 -o %(title)s.%(ext)s -i "+id);
+            StreamGobbler errorGobbler = new StreamGobbler(p.getErrorStream(), "ERROR");
+            StreamGobbler outputGobbler = new StreamGobbler(p.getInputStream(), "OUTPUT");
+
+            errorGobbler.start();
+            outputGobbler.start();
+
             int status = p.waitFor();
             if (status != 0)
                 System.err.println("WHOOPS "+status);
