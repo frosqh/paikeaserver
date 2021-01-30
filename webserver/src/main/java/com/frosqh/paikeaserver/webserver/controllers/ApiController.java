c3b4c5d84f2947ff4014db1ee76c1b665d2a4a72
Gautier Raimondi <gautier.raimondi@inria.fr>
2021-01-30 18:11:44 +0100

    Debug apiinfos

--- webserver/src/main/java/com/frosqh/paikeaserver/webserver/controllers/ApiController.java
+++ webserver/src/main/java/com/frosqh/paikeaserver/webserver/controllers/ApiController.java
@@ -4,12 +4,13 @@ import com.frosqh.paikeaserver.webserver.PaikeaApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 public class ApiController {
 
     @RequestMapping("/apiinfos")
